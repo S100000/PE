abff5046928e02f35da957ca9c2b5718cc8f47f4
José Luiz <redg7338@gmail.com>
2024-09-13 14:26:50 -0300

    finalizado ex 2

--- AlocacaoDinamica/ex2.c
+++ AlocacaoDinamica/ex2.c
@@ -3,6 +3,7 @@
 //Aloque espaço para uma seqüência de números reais escolhida pelo usuário. Receba os valores
 //e imprima-os em ordem inversa. Utilizar função para alocação dinâmica.
 void aloca(float **p, int tam);
+void leitura(float *p, int tam);
 void reverse(float *p, int tam);
 
 int main()
@@ -11,7 +12,8 @@ int main()
 	float *ptr = NULL;
 	printf("digite quantos numeros vc gostaria que fosse impressos: ");
 	scanf("%i", &tam);
-	alocar(&ptr, tam);
+	aloca(&ptr, tam);
+	leitura(ptr, tam);
 	reverse(ptr, tam);
 	
 	return 0;
@@ -22,5 +24,24 @@ void aloca(float **p, int tam)
 	if((*p=(float *)realloc(*p, tam*sizeof(float))) == NULL)
 	{
 		exit(1);
+	}
+}
+
+void leitura(float *p, int tam)
+{
+	int i;
+	for(i = 0; i < tam; i ++)
+	{
+		printf("\nDigite um numero: ");
+		scanf("%f", p+i);
+	}
+}
+
+void reverse(float *p, int tam)
+{
+	int i;
+	for(i = tam-1; i >= 0; i --)
+	{
+		printf("\n%.2f", *(p+i));
 	}
 }
